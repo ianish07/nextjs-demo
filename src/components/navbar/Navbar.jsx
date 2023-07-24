9b15c2ad8e8bb1d43da2d63bb2befb47aaae28b6
Kumar Anish <anish@Kumars-MacBook-Air.local>
2023-07-24 15:15:59 +0530

    Added DarkModeToggle

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react'
 import styles from "./navbar.module.css"
+import DarkModeToggle from '../DarkModeToggle/DarkModeToggle';
 
 const links = [
     {
@@ -40,6 +41,7 @@ const Navbar = () => {
     <div className={styles.container}>
         <Link href='/' className={styles.logo}>KarmaYogi</Link>
         <div className={styles.links}>
+          <DarkModeToggle />
             {links.map( link => (
                 <Link key={link.id} href={link.url}>
                     {link.title}
