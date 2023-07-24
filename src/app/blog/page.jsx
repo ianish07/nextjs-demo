c6bc7d70cf000937dac8a5ef0fee1be2462c661e
Kumar Anish <anish@Kumars-MacBook-Air.local>
2023-07-24 15:04:11 +0530

    Added UI

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import styles from "./page.module.css";
+import Link from 'next/link'
+import Image from 'next/image';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={styles.mainContainer}>
+      <Link href='/blog/test' className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image 
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+            />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href='/test' className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image 
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+            />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
